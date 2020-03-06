f33c60b6254faa7eda2b79ffb0bcebf52a157026
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-03-05 21:06:11 -0600

    Add models for project

---
+++ app/src/main/java/com/example/mypets/models/Dog.java
@@ -0,0 +1,25 @@
+package com.example.mypets.models;
+
+public class Dog extends Pet {
+    private String name;
+    private int age;
+
+    private long id; //Database ID
+
+    public Dog(String name, int age){
+        super(name, age);
+    }
+
+    //Constructor when initialized from Database
+    public Dog(String name, int age, long id){
+        super(name, age, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" +
+                "nombre='" + name + '\'' +
+                ", edad=" + age +
+                '}';
+    }
+}
